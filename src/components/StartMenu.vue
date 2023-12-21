2339fce34968d18040989549853c2284345fbf61
windoge98 <nolan@syslogica.io>
2023-12-21 04:47:25 -0600

    fix hover color

--- src/components/StartMenu.vue
+++ src/components/StartMenu.vue
@@ -138,14 +138,13 @@ function isSubMenuVisible(item: StartMenuItem) {
             "
           >
             <span>{{ item.name }}</span>
-            <span v-if="item.submenu" style="margin-right: 0.8rem; margin-top: 2px"
-              ><svg
+            <span v-if="item.submenu" class="submenu-expand-icon">
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="16"
                 width="8"
                 viewBox="0 0 256 512"
               >
-                <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
                 <path
                   d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"
                 /></svg
@@ -183,6 +182,18 @@ function isSubMenuVisible(item: StartMenuItem) {
 </template>
 
 <style scoped>
+  .submenu-expand-icon {
+    margin-right: 0.8rem;
+    margin-top: 2px;
+    cursor: pointer; /* Optional, to change the mouse cursor on hover */
+  }
+
+  .start-menu-item:hover svg {
+    fill: white;
+    stroke: white;
+    color: white;
+  }
+
 .start-menu-item-icon {
   float: left;
   margin-right: 10px;
